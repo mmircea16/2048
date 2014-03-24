ccfc159a970e65af386090061ffb30449a2ada84
Mircea Moise <mmoise@thoughtworks.com>
2014-03-24 22:37:30 +0000

    Properly colored tiles

--- js/game_manager.js
+++ js/game_manager.js
@@ -131,7 +131,7 @@ GameManager.prototype.moveTile = function (tile, cell) {
 };
 
 GameManager.prototype.consecutive = function(a,b){
-    var fibonacci = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
+    var fibonacci = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
     return ((Math.abs(fibonacci.indexOf(a) - fibonacci.indexOf(b)) == 1) || (a===1 && b===1));
 }
 
--- js/tile.js
+++ js/tile.js
@@ -3,6 +3,9 @@ function Tile(position, value) {
   this.y                = position.y;
   this.value            = value || 2;
 
+  var fibonacci = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+
+  this.index = fibonacci.indexOf(value)
   this.previousPosition = null;
   this.mergedFrom       = null; // Tracks tiles that merged together
 }
